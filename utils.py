ae6e38b2f2eb043ddaf8fa665527882d418df2f7
Henry Dampier <93614009+HenryDevelopsThings@users.noreply.github.com>
2025-05-21 21:34:05 +0100

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-import random
-
-def get_grid_center_position() -> pygame.Vector2:
-    return pygame.Vector2(SCREEN_WIDTH//2, SCREEN_HEIGHT//2)
-def get_random_grid_position() -> pygame.Vector2:
-    return pygame.Vector2(random.randrange(0, SCREEN_WIDTH, 10), random.randrange(0, SCREEN_HEIGHT, 10))
